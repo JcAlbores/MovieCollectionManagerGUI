20a0d75c80b41c9f5ed5a6c6a3b7202acb80237e
JcAlbores <53365606+JcAlbores@users.noreply.github.com>
2026-01-25 17:31:28 +1100

    added setter for subject field

--- src/core/Documentary.java
+++ src/core/Documentary.java
@@ -7,18 +7,24 @@ package core;
 // -- This class inherits or extends collection class
 public class Documentary extends Collection {
 	// -- Variable that stores what the documentary is about
-    private final String subject;
+    private String subject;
     
     // -- A constructor used to create a new documentary object
     public Documentary(int id, String title, String genre, int year, double rating, String subject) {
     	// -- Super calls the constructor of the parent class (collection)
         super(id, title, genre, year, rating);
-        this.subject = subject;
+        setSubject(subject);
     }
     // -- Getter method for "subject" 
     public String getSubject() {
         return subject;
     }
+    
+    // Setter for Subject
+    public void setSubject(String subject) {
+    	this.subject = subject;
+    }
+    
     // -- This method override the parent class's getcategory() method  
     @Override
     public String getCategory() {

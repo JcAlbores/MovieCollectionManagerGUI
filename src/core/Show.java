8277f2147af85ee48341ff319d3747e04026fe38
JcAlbores <53365606+JcAlbores@users.noreply.github.com>
2026-01-25 14:38:04 +1100

    create setter method for ep and season

--- src/core/Show.java
+++ src/core/Show.java
@@ -7,9 +7,9 @@ package core;
 // -- Extends the Collection class
 public class Show extends Collection {
 	// -- "Seasons" stores how many seasons the show has
-    private final int seasons;
+    private int seasons;
     // -- "Episodes" stores how many episodes the show has
-    private final int episodes;
+    private int episodes;
 
     // -- Constructor used to create a new show object with all the details
     // -- It takes both the shared attributes (from Collection) and new ones specific to shows
@@ -18,8 +18,8 @@ public class Show extends Collection {
     	// -- Sets up common information like id, title, genre, year, and rating
         super(id, title, genre, year, rating);
         // -- Assigns the show-specific values for seasons and episodes
-        this.seasons = seasons;
-        this.episodes = episodes;
+        setSeasons(seasons);
+        setEpisodes(episodes);
     }
     // -- Getter method: returns how many seasons the show has
     public int getSeasons() { 
@@ -29,6 +29,16 @@ public class Show extends Collection {
     public int getEpisodes() { 
         return episodes; 
     }
+    
+    // Setter methods for seasons and episodes:
+    public void setSeasons(int seasons) {
+    	this.seasons = seasons;
+    }
+    
+    public void setEpisodes(int episodes) {
+    	this.episodes = episodes;
+    }
+    
     // -- Overrides the getCategory() method from the parent class
     // -- This ensures that when called on a Show object, it returns "Show"
     // helping the program identify this as a TV show or series type

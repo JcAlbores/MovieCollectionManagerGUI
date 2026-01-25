a9f944919ff4e482267420de749a593fafb3f3ec
JcAlbores <53365606+JcAlbores@users.noreply.github.com>
2026-01-25 14:35:37 +1100

    include setter method for movietype

--- src/core/Movie.java
+++ src/core/Movie.java
@@ -8,18 +8,24 @@ package core;
 
 public class Movie extends Collection {
 	// -- Declared as final so once set in the constructor, it cannot be changed
-    private final String movieType;
+    private String movieType;
     // -- Constructor used to create a new movie object with all its details 
     public Movie(int id, String title, String genre, int year, double rating, String movieType) {
     	// -- Calls the parent class (Collection) constructor
     	// -- Passes the shared attribute (id, title, genre, year, rating) to the parent class handle
         super(id, title, genre, year, rating);
-        this.movieType = movieType;
+        setMovieType(movieType);
     }
     // -- Getter method: returns the type of movie
     public String getMovieType() {
         return movieType;
     }
+    
+    // Setter for movieType
+    public void setMovieType(String movieType) {
+    	this.movieType = movieType;
+    }
+    
     // -- Override the getCategory() method from the parent class
     // -- Ensures that when called on a movie object, it always returns "Movie" 
     @Override
